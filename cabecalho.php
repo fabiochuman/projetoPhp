6426b3751a47d7cc28132269c6fb92008cd5a093
Fabio Chuman <fabio_chuman@icloud.com>
2017-06-05 12:47:15 -0300

    - Adicionando .idea, bootstrap, cabecalho, fncoes, informacao e rodape

---
+++ cabecalho.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<?php
+    require_once "Pessoa.php";
+    $clientes = array(new Pessoa("Fabio","123456789","Rua X, 10"),new Pessoa("Jose","786423165","Rua Y, 20"), new Pessoa("Elaine","451789245","Rua X, 10"),
+        new Pessoa("Christopher","897613254","Rua A, 18"), new Pessoa("Elias","894516754","Rua F, 156"), new Pessoa("Xilovaldo","651497856","Rua H, 8"),
+        new Pessoa("Marcella","123459746","Rua F, 156"), new Pessoa("Melisandra","004879542","Rua X, 10"),new Pessoa("Antonio","78945613x","Rua T, 789"),
+        new Pessoa("Erick","782364722","Rua A, 18"));
+?>
+<html>
+    <head>
+        <title>Projeto PHP - Tabela de Clientes</title>
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.css">
+        <link rel="stylesheet" href="bootstrap/css/estilo.css"
+    </head>
+    <body>
+    <table class="table table-bordered table-striped" style="background-color: white;">
+        <thead>
+        <tr>
+            <th>CPF</th>
+            <th>NOME</th>
+        </tr>
+        </thead>
+        <tbody>
---
+++ informacao.php
@@ -0,0 +1,14 @@
+<?php
+    include("cabecalho.php");
+    $cpf = $_GET['cpf'];
+    echo $cpf;
+?>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+            </tbody>
+        </table>
+    </body>
+</html>
